032068ea6eb6dc57e333a9007245d2bf48a44441
Anubhav Kumar <anubhav11697@gmail.com>
2023-01-25 00:23:52 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import Header from './components/Header';
+import Home from './components/Home';
 import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      Back on a new project
+      <Header />
+      <Home />
+      <Footer />
     </div>
   );
 }
