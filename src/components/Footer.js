f32d22933a56422e8eadb640169815e56ecfcfac
Anubhav Kumar <anubhav11697@gmail.com>
2023-01-24 23:40:17 +0530

    Frontend component setup

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Footer() {
+  return (
+    <Container>
+        Footer component
+    </Container>
+  )
+}
+
+export default Footer
+
+const Container=styled.div`
+    
+`
