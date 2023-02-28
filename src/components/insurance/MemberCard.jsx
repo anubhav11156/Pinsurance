9232eab4e60e2438b0208aaa54cb2a71dc2eb193
Anubhav Kumar <anubhav11697@gmail.com>
2023-03-01 02:08:24 +0530

    UI: Pool members section

--- src/components/insurance/MemberCard.jsx
+++ src/components/insurance/MemberCard.jsx
@@ -2,15 +2,206 @@ import React from 'react'
 import styled from 'styled-components'
 
 function MemberCard() {
-  return (
-    <Container>
-        this is for member card
-    </Container>
-  )
+    return (
+        <Container>
+            <div className='left'>
+                <div className='profile-image'>
+                    <img src="https://bafybeifzpfapxzucdrfbr3n4ogml72vxyrxf2kkk7l5mh2aumxkmk7qtoi.ipfs.w3s.link/thelvedem.jpg" />
+                </div>
+            </div>
+            <div className='right'>
+                <div className='detail-container'>
+                    <div className='name-div'>
+                        <p>Anubhav Kumar</p>
+                    </div>
+                    <div className='address-div'>
+                        <div className='address'>
+                            <p>0x9d211d45e432c43b4F8b6Eb86c841A11cFc5AD90</p>
+                        </div>
+                    </div>
+                    <div className='meta-div'>
+                        <div className='email-div'>
+                            <div className='logo-div'>
+                                <img src='/images/gmail.png' />
+                            </div>
+                            <div className='email'>
+                                <p>anubhav11697@gmail.com</p>
+                            </div>
+                        </div>
+                        <div className='premium-div'>
+                            <div className='logo-div'>
+                                <img src='/images/usdc-logo.svg' />
+                            </div>
+                            <div className='email'>
+                                <p>3500</p>
+                            </div>
+                        </div>
+                        <div className='vehicle-div'>
+                            <div className='logo-div'>
+                                <img src='/images/car.png' />
+                            </div>
+                            <div className='email'>
+                                <p>Supra</p>
+                            </div>
+                        </div>
+                        <div className='cc-div'>
+                            <div className='logo-div'>
+                                <img src='/images/engine.png' />
+                            </div>
+                            <div className='email'>
+                                <p>1000</p>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </Container>
+    )
 }
 
 export default MemberCard
 
 const Container = styled.div`
+    height: 98.7%;
+    width: 99%;
+    border-radius:5px;
+    border: 1px solid #0152b565;
+    background-color: #0152b51f;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 
+    .left {
+        width: 7rem;
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        .profile-image {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            width: 5rem;
+            height: 5rem;
+            border-radius: 3px;
+            border: 1px solid black;
+            overflow: hidden;
+            
+            img {
+                width: 100%;
+            }
+        }
+    }
+
+    .right {
+      flex: 1;
+      height: 100%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      .detail-container {
+        display: flex;
+        flex-direction: column;
+        width: 100%;
+        height: 5rem;
+        gap: 7px;
+        .address-div {
+            height: 1.5rem;
+            width: 98.5%;
+            display: flex;
+            align-items: center;
+            border: 1px solid white;
+            background-color: #ffffff83;
+            border-radius: 3px;
+
+            .address {
+                flex: 1;
+                height: 100%;
+                display: flex;
+                justify-content: start;
+                align-items: center;
+
+                p {
+                    margin: 0;
+                    margin-left: 10px;
+                    font-size: 15px;
+                }
+            }
+
+            .stake-div {
+                width: 10rem;
+                height: 100%;
+                background-color: lightblue;
+            }
+        }
+
+        .name-div {
+            flex:1.2;
+            width: 98.5%;
+            display: flex;
+            justify-content: start;
+            align-items: center;
+            border: 1px solid white;
+            background-color: #ffffff83;
+            border-radius: 3px;
+
+            p {
+                margin: 0;
+                margin-left: 10px;
+                font-size: 24px;
+            }
+        }
+
+        .meta-div {
+            height: 1.5rem;
+            width: 98.5%;
+            display: flex;
+            align-items: center;
+            gap: 7px;
+
+            .email-div,
+            .premium-div,
+            .vehicle-div,
+            .cc-div {
+                /* margin-left: 10px; */
+                flex:1;
+                height: 100%;
+                border-radius: 3px;
+                display: flex;
+                justify-content:start;
+                align-items: center;
+                overflow: hidden;
+                background-color: #ffffff83;
+                border: 1px solid white;
+
+                .logo-div {
+                    width: 2rem;
+                    height: 100%;
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    border-right: 2px solid white;
+
+                    img {
+                        width: 50%;
+                    }
+                }
+
+                .email {
+                    display: flex;
+                    justify-content: start;
+                    align-items: center;
+                    p {
+                        margin: 0;
+                        font-size: 15px;
+                        margin-left: 8px;
+                    }
+                }
+            }
+        }
+      }
+    }
 `
--- src/components/insurance/Pool.jsx
+++ src/components/insurance/Pool.jsx
@@ -7,6 +7,7 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import ClipLoader from "react-spinners/ClipLoader";
 import fromExponential from 'from-exponential';
+import MemberCard from './MemberCard';
 
 
 function Pool() {
@@ -17,9 +18,14 @@ function Pool() {
   const [poolBalance, setPoolBalance] = useState();
   const [poolName, setPoolName] = useState('');
   const [isPoolActive, setIsPoolActive] = useState(false);
+  const [isDataFetched, setIsDataFetched] = useState(false);
 
   const hexToDec = (hex) => parseInt(hex, 16);
 
+
+  useEffect(() => {
+    setIsDataFetched(false);
+  }, [poolAddress]);
 
   /*-----------------------------------------get pool members------------------------------*/
 
@@ -93,6 +99,7 @@ function Pool() {
           setIsPoolActive(response.isActive);
           setPoolName(response.name);
           setIsSearching(false);
+          setIsDataFetched(true);
         })
     } catch (error) {
       console.log(error);
@@ -131,9 +138,71 @@ function Pool() {
           }
         </div>
       </div>
-      <div className='down'>
-        for member card
-      </div>
+
+      {!isDataFetched &&
+        <div className='down'>
+          <div className='pool-detail'>
+            <div className='upper'>
+              {/* <p>Pool Name</p> */}
+              <div className='name-label'>
+                <p>Pool Name</p>
+              </div>
+              <div className='name'>
+                <p>{poolName}</p>
+              </div>
+            </div>
+            <div className='lower'>
+              <div className='balance-div'>
+                <div className='label'>
+                  <p>Pool Balance</p>
+                </div>
+                <div className='text'>
+                  <p>{poolBalance}</p>
+                </div>
+              </div>
+              <div className='members-div'>
+                <div className='label'>
+                  <p>Members</p>
+                </div>
+                <div className='text'>
+                  <p>{members.length}/3</p>
+                </div>
+              </div>
+              <div className='status-div'>
+                <div className='label'>
+                  <p>Status</p>
+                </div>
+                <div className='text'>
+                  { isPoolActive &&
+                    <p>Pool is active</p>
+                  }
+                  { !isPoolActive &&
+                    <p>Pool is NOT yet active</p>
+                  }
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='members-container'>
+            <div className='member-label'>
+                <p>Pool members </p>
+            </div>
+            <div className='main'>
+              <div className='member-box'>
+                <MemberCard />
+              </div>
+              <div className='member-box'>
+                <MemberCard />
+              </div>
+              <div className='member-box'>
+                <MemberCard />
+              </div>
+            </div>
+            
+          </div>
+        </div>
+      }
+
     </Container>
   )
 }
@@ -210,6 +279,139 @@ const Container = styled.div`
     width: 96%;
     flex: 1;
     margin-bottom: 1.2rem;
-    background-color: lightgreen;
+    display: flex;
+    flex-direction: column;
+    overflow: hidden;
+    border-radius: 5px;
+    border: 1px solid #0152b565;
+    gap: 1rem;
+    
+    .pool-detail {
+      width: 100%;
+      height: 5rem;
+      display: flex;
+      flex-direction: column;
+
+      .upper {
+        flex: 1;
+        display: flex;
+        border-bottom: 1px solid #0152b565;
+
+        .name-label {
+          height: 100%;
+          width: 10rem;
+          background-color: #0152b5cc;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+
+          p {
+            margin: 0;
+            color: white;
+            font-size: 17px
+          }
+        }
+
+        .name {
+          flex:1;
+          display: flex;
+          justify-content: start;
+          align-items: center;
+
+          p {
+           margin:0;
+           font-size: 17px;
+           margin-left: 15px;
+           color: #202020f1;
+          }
+
+        }
+       
+      }
+
+      .lower {
+        flex:1;
+        display: flex;
+        align-items: center;
+        border-bottom: 1px solid #0152b565;
+
+
+        .balance-div,
+        .members-div,
+        .status-div {
+          flex: 1;
+          height: 100%;
+          display: flex;
+          align-items: center;
+
+          .label {
+            width: 10rem;
+            height: 100%;
+            background-color: #0152b5cc;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+
+            p {
+              margin: 0;
+              color: white;
+              font-size: 17px
+            }
+          }
+
+          .text {
+            flex:1;
+            display: flex;
+            justify-content: start;
+            align-items: center;
+
+              p {
+                margin:0;
+                font-size: 17px;
+                margin-left: 15px;
+                color: #202020f1;
+              }
+            }
+        }
+
+       
+      }
+    }
+
+    .members-container {
+      flex:1;
+      width: 100%;
+      display:flex;
+      flex-direction: column;
+      align-items: center;
+
+      .member-label {
+        width: 98%;
+        height: 2.5rem;
+
+        p {
+          margin:0;
+          margin-left: 10px;
+          font-size: 17px;
+          color: #202020f1;
+        }
+      }
+
+      .main {
+        flex: 1;
+        width: 97%;
+        display: grid;
+        grid-template-rows: 7.5rem 7.5rem 7.5rem;
+        row-gap: 1.6rem;
+
+        .member-box {
+          height: 100%;
+          width: 100%;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+      }
+    }
   }
 `
