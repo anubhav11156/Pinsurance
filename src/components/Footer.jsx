74c7451ab139206c2d62f547e41d2ede8d71ad29
Anubhav Kumar <anubhav11697@gmail.com>
2023-03-07 23:19:36 +0530

    Footer update : added mint section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,108 @@
-import React from 'react'
+import { React, useState } from 'react'
 import styled from 'styled-components'
-
+import { ethers } from 'ethers';
+import { mockUsdcContractAddress, mockUsdcAbi } from "../config";
+import ClipLoader from "react-spinners/ClipLoader";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import copy from 'copy-to-clipboard';
 
 function Footer() {
+
+  const [isMinting, setIsMinting] = useState(false);
+  const [address, setAddress] = useState();
+
+  const ownerPrivateKey = process.env.REACT_APP_PRIVATE_KEY;
+
+  const ownerAddress = "0x22b6Dd4D6d818e2Ebce3D2E009A249F8FbF4e965";
+  const value = "5000"; // testing for 1 usdc
+
+  const copyAddress =  () => {
+    copy('0xF8E9F063228eb47137101eb863BF3976466AA31F');
+  }
+
+  const mintFake = async () => {
+    setIsMinting(true);
+    const provider = new ethers.providers.JsonRpcProvider('https://endpoints.omniatech.io/v1/fantom/testnet/public');
+    const wallet = new ethers.Wallet(ownerPrivateKey);
+    const signer = wallet.connect(provider);
+    const usdcContract = new ethers.Contract(
+      mockUsdcContractAddress,
+      mockUsdcAbi.abi,
+      signer
+    )
+
+    try {
+      const amount = ethers.utils.parseEther(value);
+      const approveTx = await usdcContract.approve(
+        ownerAddress,
+        amount
+      )
+
+      await approveTx.wait()
+        .then(() => {
+          transferUsdc();
+        }).catch((e) => {
+          setIsMinting(false);
+          toast.error("Transaction failed!", {
+            position: toast.POSITION.TOP_CENTER
+          });
+        })
+    } catch (error) {
+      setIsMinting(false);
+      toast.error("Transaction failed!", {
+        position: toast.POSITION.TOP_CENTER
+      });
+    }
+  }
+
+  const transferUsdc = async () => {
+    const provider = new ethers.providers.JsonRpcProvider('https://endpoints.omniatech.io/v1/fantom/testnet/public');
+    const wallet = new ethers.Wallet(ownerPrivateKey);
+    const signer = wallet.connect(provider);
+    const usdcContract = new ethers.Contract(
+      mockUsdcContractAddress,
+      mockUsdcAbi.abi,
+      signer
+    )
+
+    try {
+      const amount = ethers.utils.parseEther(value);
+      const tx = await usdcContract.transferFrom(
+        ownerAddress,
+        address,
+        amount
+      )
+
+      await tx.wait()
+        .then(() => {
+          toast.success("Fake USDC received.", {
+            position: toast.POSITION.TOP_CENTER
+          });
+          setIsMinting(false);
+        }).catch((e) => {
+          console.log(e);
+          setIsMinting(false);
+        })
+
+
+    } catch (error) {
+      setIsMinting(false);
+      toast.error("Transaction failed!", {
+        position: toast.POSITION.TOP_CENTER
+      });
+    }
+
+  }
+
   return (
     <Container>
-      <div className="metropolis">
+      <div className="pinsurance">
         <div className="text-container">
-          <p className="my-metropolis">Pinsurance</p>
+          <p className="my-pinsurance">Pinsurance</p>
         </div>
       </div>
-      <a href="https://github.com/anubhav11156/Pinsurance-FVM-Spacewarp" className="source-code-div" target="_blank">
+      <a href="https://github.com/anubhav11156/Pinsurance" className="source-code-div" target="_blank">
         <div>
           <img src="/images/github-logo.png" />
         </div>
@@ -57,6 +149,27 @@ function Footer() {
           </div>
         </div>
       </div>
+      <div className='mint-div'>
+        <div className='heading'>
+          <p>Import & mint Fake USDC for testing this dapp.</p>
+        </div>
+        <div className='mint-button' onClick={copyAddress}>
+            <p>Click to copy fake USDC contract address. </p>
+        </div>
+        <div className='input-div'>
+          <input type="text" placeholder='Address' onChange={(e) => {
+            setAddress(e.target.value);
+          }} />
+        </div>
+        <div className='mint-button' onClick={mintFake}>
+          {!isMinting &&
+            <p>Mint 5000 USDC </p>
+          }
+          {isMinting &&
+            <ClipLoader color="#ffffff" size={13} />
+          }
+        </div>
+      </div>
     </Container>
   )
 }
@@ -64,8 +177,8 @@ function Footer() {
 export default Footer
 
 const Container = styled.div`
+  position: relative;
   height: 600px;
-  background-color: lightpink;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -74,7 +187,7 @@ const Container = styled.div`
 
   background-color: rgba(10,10,13,255);
 
-  .metropolis {
+  .pinsurance {
     padding-left: 20px;
     flex:1;
     display: flex;
@@ -89,7 +202,7 @@ const Container = styled.div`
       flex-direction: column;
       width: 600px;
 
-      .my-metropolis {
+      .my-pinsurance {
         margin: 0;
         font-size: 88px;
         font-weight: 600;
@@ -223,5 +336,84 @@ const Container = styled.div`
      font-size: 16px;
      margin-left: 5px;
    }
+ }
+
+ .mint-div {
+  position: absolute;
+  margin-top: 3.5rem;
+  top: 2rem;
+  left: 2rem;
+  width: 30rem;
+  height: 16rem;
+  background: rgba( 255, 255, 255, 0.1 );
+  backdrop-filter: blur( 4px );
+  -webkit-backdrop-filter: blur( 4px );
+  border-radius: 2px;
+  border: 2px solid rgba( 255, 255, 255, 0.5 );
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  .heading {
+    flex:1;
+    width: 90%;
+    display: flex;
+    justify-content: start;
+    align-items: center;
+
+    p {
+      margin:0;
+      font-size: 16px;
+      color: rgba(255, 255, 255, 0.5);
+      letter-spacing: 1px;
+    }
+  }
+
+  .input-div {
+    flex: 1;
+    width: 90%;
+
+    input {
+      width: 95%;
+      height: 2rem;
+      border-radius: 5px;
+      outline: none;
+      border: 2px solid rgba( 255, 255, 255, 0.5 );
+      padding-left: 10px;
+
+      font-size: 15px;
+
+    }
+  }
+
+  .mint-button {
+    height: 3rem;
+    width: 90%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: black;
+    border-radius: 5px;
+    margin-bottom: 1rem;
+    cursor: pointer;
+    transition: color 0.15s;
+
+    color: #ffffffe0;
+
+
+    &:hover {
+      color: #3adfae;
+    }
+
+    &:active {
+      color: #3adfaee2;
+    }
+
+    p {
+      margin: 0;
+    }
+  }
+
  }
 `
